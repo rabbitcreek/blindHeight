59ffb4ac0a5e8957e09f30e6366e41c4158e4fc2
rabbitcreek <rabbitcreek@gmail.com>
2021-01-03 17:56:39 -0900

    Add files via upload

---
+++ finalblindFunnel.ino
@@ -0,0 +1,32 @@
+#define pwmPin  0
+int analogInPin = A1;
+float basePressure = 0.0;
+float voltage = 0.0;
+float pressure = 0.0;
+void setup(){
+pinMode(pwmPin, OUTPUT);
+pinMode(analogInPin, INPUT);
+ voltage = 3.3*float((analogRead(A1)/1023.0));
+ pressure = 5*((voltage - 0.2)/3.3);
+basePressure = pressure;
+for (int i = 0; i < 3; i++){
+digitalWrite(0,HIGH ); // Output analog reading to dimmable LED
+delay(20);
+digitalWrite(0,LOW);
+delay(20);
+}
+}
+void loop(){
+ voltage = 3.3*float((analogRead(A1)/1023.0));
+ pressure = 5*((voltage - 0.2)/3.3);
+float dong = 10 * (pressure - basePressure);
+int counter = map((dong),0,4,100,10);
+counter = constrain(counter,10,100 );
+//int counter = map((pressure * 10),1,15,100,10);
+if (pressure - basePressure > 0.15){
+digitalWrite(0,HIGH ); // Output analog reading to dimmable LED
+delay(counter);
+digitalWrite(0,LOW);
+delay(counter);
+}
+}
